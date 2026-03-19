62c35151ff2e6dc47913f0593dc0cf65358a0628
Deepanshu gupta <deepanshu.gupta.cse.2024@miet.ac.in>
2026-03-19 11:54:22 +0530

    Fix weather app code and API handling
    Fix temperature and weather print statements with error handling.

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ api_Key = "your_api_Key_here"
 url =
 f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_Key}&units=metric"
 data = requests.get(url).json()
-print("Tewmperature:", data["main"]
+if data.get("main"):
+      print("Tewmperature:", data["main"]
       ["temp"])
-print("Weather:", data["weather"][0]
+      print("Weather:", data["weather"][0]
       ["description"])
+else:
+      print("Errot:", data)
